4d4cf5d933b1a8efc985911da330f9d380856126
Anna <46578108+anchsk@users.noreply.github.com>
2025-05-27 09:25:49 +0200

    improved conversation flow

--- chat-app/chatbot.py
+++ chat-app/chatbot.py
@@ -9,26 +9,31 @@ tokenizer = AutoTokenizer.from_pretrained(model_name) # optimizes the input
 
 conversation_history = []
 
-history_string = "\n".join(conversation_history)
+while True:
 
-input_text = "hello, how are you doing?"
+    history_string = "\n".join(conversation_history)
 
-inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
+    input_text = input("> ")
+    """ Hello, how are you doing? """
 
-print(inputs)
-""" {'input_ids': tensor([[1710,   86,   19,  544,  366,  304,  929,   38]]), 'attention_mask': tensor([[1, 1, 1, 1, 1, 1, 1, 1]])} """
+    inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
 
-outputs = model.generate(**inputs)
-print(outputs)
-""" tensor([[   1,  281,  476,  929,  731,   21,  281,  632,  929,  712,  731,   21,
-          855,  366,  304,   38,  946,  304,  360,  463, 5459, 7930,   38,    2]]) """
-   
-   
-response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
-print(response)
-       
-""" I'm doing well. I am doing very well. How are you? Do you have any hobbies? """
+    print(inputs)
+    """ {'input_ids': tensor([[1710,   86,   19,  544,  366,  304,  929,   38]]), 'attention_mask': tensor([[1, 1, 1, 1, 1, 1, 1, 1]])} """
 
-conversation_history.append(input_text)
-conversation_history.append(response)
-print(conversation_history)
+    outputs = model.generate(**inputs)
+    print(outputs)
+    """ tensor([[   1,  281,  476,  929,  731,   21,  281,  632,  929,  712,  731,   21,
+              855,  366,  304,   38,  946,  304,  360,  463, 5459, 7930,   38,    2]]) """
+    
+    
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
+    print(response)
+
+    """ I'm doing well. I am doing very well. How are you? Do you have any hobbies? """
+
+    conversation_history.append(input_text)
+    conversation_history.append(response)
+    print(conversation_history)
+
+# Press ctrl + c to exit
